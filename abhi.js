71f06fc8b66be548f0a2829e3ee852314b29de68
Abhijeet <lex.abhijeet15@gmail.com>
2025-07-02 12:14:33 +0530

    initial commit

---
+++ abhi.js
@@ -0,0 +1,14 @@
+function shareLink(link) {
+  if (navigator.share) {
+    navigator.share({
+      title: 'Check this out!',
+      url: link
+    }).catch((error) => {
+      console.error('Error sharing:', error);
+    });
+  } else {
+    navigator.clipboard.writeText(link)
+      .then(() => alert('Link copied to clipboard!'))
+      .catch(() => alert('Failed to copy link'));
+  }
+}
